b7212fc2e80153643dea8110a8194dea322790df
Art8m <9artbe@gmail.com>
2022-11-25 04:10:40 -0800

    stetic

---
+++ Source/Applications/Desktop/Vodovoz/Views/Reports/EdoUpdReportView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vodovoz.Views.Reports
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class EdoUpdReportView : Gtk.Bin
+	{
+		public EdoUpdReportView()
+		{
+			this.Build();
+		}
+	}
+}
---
+++ Source/Applications/Desktop/Vodovoz/gtk-gui/Vodovoz.Views.Reports.EdoUpdReportView.cs
@@ -0,0 +1,56 @@
+
+namespace Vodovoz.Views.Reports
+{
+	public partial class EdoUpdReportView
+	{
+		private Gtk.HBox hbox1;
+
+		private Gtk.HBox hboxMain;
+
+		private Gtk.VBox parametersContainer;
+
+		private Gtk.Table table1;
+
+		private Gamma.GtkWidgets.yLabel ylabel3;
+
+		private Gamma.GtkWidgets.yHBox yhboxType;
+
+		private Gtk.Frame frameType;
+
+		private Gtk.Alignment GtkAlignmentInterval;
+
+		private Gamma.GtkWidgets.yVBox yvboxType;
+
+		private Gtk.RadioButton ycheckEdoSuccess;
+
+		private Gtk.RadioButton ycheckMissing;
+
+		private Gtk.Label labelType;
+
+		private QS.Widgets.GtkUI.DateRangePicker rangeDate;
+
+		private Gamma.GtkWidgets.yButton ybuttonCreateReport;
+
+		private Gamma.GtkWidgets.yButton ybuttonSave;
+
+		private Gtk.EventBox eventboxArrow;
+
+		private Gtk.VBox vbox4;
+
+		private Gtk.VSeparator vseparator1;
+
+		private Gtk.Arrow arrowSlider;
+
+		private Gtk.Label labelTitle;
+
+		private Gtk.VSeparator vseparator2;
+
+		private Gtk.ScrolledWindow GtkScrolledWindow;
+
+		private Gamma.GtkWidgets.yTreeView ytreeviewReport;
+
+		private void Build()
+		{
+		}
+	}
+}
