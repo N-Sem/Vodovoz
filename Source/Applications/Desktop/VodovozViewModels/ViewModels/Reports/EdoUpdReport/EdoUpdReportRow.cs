8ba124db7225510105f782bcbf481bb9d11f37ee
Art8m <9artbe@gmail.com>
2022-11-25 17:10:03 +0300

    Отчёт

---
+++ Source/Applications/Desktop/VodovozViewModels/ViewModels/Reports/EdoUpdReport/EdoUpdReportRow.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Gamma.Utilities;
+using Vodovoz.Domain.Client;
+using Vodovoz.Domain.Orders.Documents;
+
+namespace Vodovoz.ViewModels.ViewModels.Reports.EdoUpdReport
+{
+	public class EdoUpdReportRow
+	{
+		public string Inn { get; set; }
+		public string CounterpartyName { get; set; }
+		public string OrderId { get; set; }
+		public DateTime UpdDate { get; set; }
+		public string UpdDateString => UpdDate.ToString("g");
+		public string Gtin { get; set; }
+		public decimal Count { get; set; }
+		public decimal Price { get; set; }
+		public decimal Sum { get; set; }
+		public EdoDocFlowStatus EdoDocFlowStatus { get; set; }
+		public string EdoDocFlowStatusString => EdoDocFlowStatus.GetEnumTitle();
+		public string TrueMarkApiError { get; set; }
+		public bool IsTrueMarkApiSuccess { get; set; }
+	}
+}
